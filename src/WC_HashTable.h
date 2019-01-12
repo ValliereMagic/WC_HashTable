75538b318b4c2d289aec67ad60c44080ab800670
ValliereMagic <louisemelaney@outlook.com>
2019-01-12 16:21:42 -0500

    Created deconstructor for hash table; created tests for making a new hash table and freeing a hash table.

--- src/WC_HashTable.h
+++ src/WC_HashTable.h
@@ -11,22 +11,22 @@
     hash_table_t* hash_table_new(void);
 
     //free a passed hash_table from memory.
-    void hash_table_free(hash_table_t* table);
+    void hash_table_free(hash_table_t* h_table);
 
     //Add the passed value to the hash table accessable by the
     //key passed.
     //values and keys will be copied into memory managed by the
     //has table.
     //function will return 1 on success, 0 on failure.
-    unsigned char hash_table_add(void* key, size_t key_length,
-                                void* value, size_t value_length);
+    unsigned char hash_table_add(hash_table_t* h_table, void* key, size_t key_length,
+                                 void* value, size_t value_length);
 
     //removes the value stored at the key passed in the hash table
     //returns 1 on success, 0 on failure.
-    unsigned char hash_table_remove(void* key, size_t key_length);
+    unsigned char hash_table_remove(hash_table_t* h_table, void* key, size_t key_length);
 
     //returns the value stored at the key passed.
     //will return null if there is nothing stored at the key passed.
-    void* hash_table_get(void* key, size_t key_length);
+    void* hash_table_get(hash_table_t* h_table, void* key, size_t key_length);
 
 #endif
--- tests/tests.c
+++ tests/tests.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "WC_HashTable.h"
 
-int main(int argc, char** argv) {
+int main(void) {
+
+    hash_table_t* new_table = hash_table_new();
+
+    hash_table_free(new_table);
 
     return 0;
 }

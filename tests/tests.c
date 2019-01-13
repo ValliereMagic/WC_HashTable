20e61baf97b426198f323c5d5faedc3e97fca17c
ValliereMagic <louisemelaney@outlook.com>
2019-01-13 17:07:19 -0500

    Created wrap_index function used by add to keep circularly itterating the table; created allocate_element function to dynamically allocate a single table element from memory; created hash_table_add function; next steps: create function to resize the hash table when it gets to max capacity.

--- tests/tests.c
+++ tests/tests.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include "WC_HashTable.h"
 
 int main(void) {
 
     hash_table_t* new_table = hash_table_new();
 
+    char* key = "Heil_Hydra";
+    char* value = "COOHTMSTIP";
+
+    hash_table_add(new_table, key, strlen(key) + 1,
+                   value, strlen(value) + 1);
+
+    hash_table_add(new_table, value, strlen(value) + 1,
+                   key, strlen(key) + 1);
+
     hash_table_free(new_table);
+
+    printf("Done.\n");
 
     return 0;
 }

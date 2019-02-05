07c2a39dbe616e6559c7342b8b7a3ee9286f8764
ValliereMagic <ValliereMagic@users.noreply.github.com>
2019-02-05 17:26:56 -0500

    Complete tests to cause a reallocation

--- tests/tests.c
+++ tests/tests.c
@@ -6,14 +6,49 @@ int main(void) {
 
     hash_table_t* new_table = hash_table_new();
 
-    char* key = "Heil_Hydra";
-    char* value = "COOHTMSTIP";
+    char* key_one = "Heil_Hydra";
 
-    hash_table_add(new_table, key, strlen(key) + 1,
-                   value, strlen(value) + 1);
+    char* value_one = "COOHTMSTIP";
 
-    hash_table_add(new_table, value, strlen(value) + 1,
-                   key, strlen(key) + 1);
+
+    char* key_two = "MSI";
+
+    char* value_two = "Micro$oft";
+
+    char* key_three = "Purple";
+
+    char* value_three = "oompaloompa";
+
+    char* key_four = "power";
+
+    char* value_four = "Of One";
+
+    hash_table_add(new_table, key_one, strlen(key_one) + 1,
+                   value_one, strlen(value_one) + 1);
+
+    hash_table_add(new_table, value_one, strlen(value_one) + 1,
+                   key_one, strlen(key_one) + 1);
+
+
+    hash_table_add(new_table, key_two, strlen(key_two) + 1,
+                   value_two, strlen(value_two) + 1);
+
+    hash_table_add(new_table, value_two, strlen(value_two) + 1,
+                   key_two, strlen(key_two) + 1);
+
+
+    hash_table_add(new_table, key_three, strlen(key_three) + 1,
+                   value_three, strlen(value_three) + 1);
+
+    hash_table_add(new_table, value_three, strlen(value_three) + 1,
+                   key_three, strlen(key_three) + 1);
+
+
+    hash_table_add(new_table, key_four, strlen(key_four) + 1,
+                   value_four, strlen(value_four) + 1);
+
+    hash_table_add(new_table, value_four, strlen(value_four) + 1,
+                   key_four, strlen(key_four) + 1);
 
     hash_table_free(new_table);
 
